80fb771e01c063efea8beb7f2e2210681d0861b2
Rachel <rachelmmarriott@gmail.com>
2022-03-26 11:57:49 -0600

    create dashboard template

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-
 const hbs = exphbs.create({ helpers });
-
-const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -22,8 +20,6 @@ const sess = {
     db: sequelize
   })
 };
-
-const helpers = require('./utils/helpers');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
